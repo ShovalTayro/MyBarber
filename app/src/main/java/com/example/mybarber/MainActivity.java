44c50fc5892f19cc402167267f92580fae18bfa6
yoantan <yonatan1231230@gmail.com>
2020-11-11 20:08:52 +0200

    test

--- app/src/main/java/com/example/mybarber/MainActivity.java
+++ app/src/main/java/com/example/mybarber/MainActivity.java
@@ -16,7 +16,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundl e savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
