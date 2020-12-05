3c76b62f132d3bd53c42714ebc7acec506f7fff7
ShovalTayro <shovaltyro6@gmail.com>
2020-12-05 18:43:03 +0200

    fix commit

---
+++ app/src/main/java/com/example/mybarber/fireBase/appointmentFB.java
@@ -0,0 +1,36 @@
+package com.example.mybarber.fireBase;
+
+import com.example.mybarber.Objects.Appointment;
+import com.google.firebase.database.DatabaseReference;
+
+public class appointmentFB extends initializeFB
+{
+    /*
+    public void addAppointmentClient(String name , String date, String time, hairCut haircut)
+    {
+        Appointment appointment = new Appointment(name,date,time,haircut);
+        //ref.child("appointment").child(getUid()).setValue(appointment);
+    }
+    */
+
+    public void addAppointmentAdmin(String date, String time)
+    {
+        Appointment appointment = new Appointment("Available",date,time, "-");
+        ref.child("appointment").child(appointment.toString()).setValue(appointment);
+    }
+
+    public DatabaseReference allAppointmentByClient(String name)
+    {
+        return (DatabaseReference) ref.child("appointment").orderByChild("name").equalTo(name);
+    }
+
+    public DatabaseReference getAppointmendByID(String id)
+    {
+        return ref.child("appointment").child(id);
+    }
+
+    public DatabaseReference allAppointments()
+    {
+        return ref.child("appointment");
+    }
+}
